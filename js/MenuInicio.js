de1590d09f344c962fbc8741fe10553ae85c1d6d
Albi52 <pozvel@gmail.com>
2024-11-18 12:38:51 +0100

    MenuInicio1

--- js/MenuInicio.js
+++ js/MenuInicio.js
@@ -5,11 +5,11 @@ class MenuInicio extends Phaser.Scene {
 
     preload() {
         //Cargo los audios con this.load.audio("path")
-		this.load.audio("select", 'assets/musica/click.mp3'); 
-        this.load.audio("background", 'assets/musica/menuMusic.mp3'); //y les pongo sus respestivas etiquetas
-
+		this.load.audio("select", "/assets/musica/click.mp3"); 
+        this.load.audio("background", "/assets/musica/menuMusic.mp3"); //y les pongo sus respestivas etiquetas
+        
         //Cargo los botones
-        //this.load.image("start_button", "assets/start-button.svg");
+        this.load.image("start_button", "/assets/botones/B_Jugar.png");
         //this.load.image("help_button", "assets/help-button.svg");
         //this.load.image("config_button", "assets/config-button.svg");
 
@@ -24,13 +24,13 @@ class MenuInicio extends Phaser.Scene {
         this.bgMusic.loop = true; //que sea loop
         this.bgMusic.play(); //que suene
 
-        //const start_button = this.add.image(400, 300, "start_button") //A�ado la imagen de start 
-        //    .setInteractive(true) /*Hago que sea interactivo*/
-        //    .on/*Hace algo mira el que*/('pointerdown', /*Creo una funcion anonima*/(/*no recibe ningun parametros*/) => {
-        //        this.sound.play("select"); //que suene el sonido de play
-        //        this.scene.stop("IntroScene"); //carga la escena de intro
-        //        this.scene.start("GameScene"); //carga la escena de game
-        //});
+        const start_button = this.add.image(400, 300, "start_button") //A�ado la imagen de start 
+            .setInteractive(true) /*Hago que sea interactivo*/
+            .on/*Hace algo mira el que*/('pointerdown', /*Creo una funcion anonima*/(/*no recibe ningun parametros*/) => {
+                this.sound.play("select"); //que suene el sonido de play
+                //this.scene.stop("IntroScene"); //carga la escena de intro
+                //this.scene.start("GameScene"); //carga la escena de game
+        });
         //const config_button = this.add.image(400, 375, "config_button");
         //const help_button = this.add.image(400, 450, "help_button");
         
