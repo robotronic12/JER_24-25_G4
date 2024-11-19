a1b44f87952a5404c7c95361ebddfef12b5bff89
Alvert04 <112902381+Alvert04@users.noreply.github.com>
2024-11-19 19:08:57 +0100

    BotonFuncional

--- js/MenuInicio.js
+++ js/MenuInicio.js
@@ -5,13 +5,14 @@ class MenuInicio extends Phaser.Scene {
 
     preload() {
         //Cargo los audios con this.load.audio("path")
-		this.load.audio("select", "assets/musica/click.mp3"); 
-        this.load.audio("background", "assets/musica/menuMusic.mp3"); //y les pongo sus respestivas etiquetas
+		this.load.audio('select', 'assets/musica/click.mp3'); 
+        this.load.audio('background', 'assets/musica/menuMusic.mp3'); //y les pongo sus respestivas etiquetas
         
         //Cargo los botones
-        this.load.image("start_button", "/assets/botones/Jugar.png");
-        //this.load.image("help_button", "assets/help-button.svg");
-        //this.load.image("config_button", "assets/config-button.svg");
+        this.load.image('start_button', 'assets/botones/Jugar.png');
+        this.load.image('opciones', 'assets/botones/Opciones.png');
+        this.load.image('salir', 'assets/botones/Salir.png');
+
 
         
     }
@@ -21,18 +22,25 @@ class MenuInicio extends Phaser.Scene {
 
         //Configuracion de la musica
         this.bgMusic = this.sound.add('background'); //pongo la musica del menu
+        this.bgMusic.setVolume(0); // Cambiar volumen (por ejemplo, 50% del volumen máximo)
         this.bgMusic.loop = true; //que sea loop
         this.bgMusic.play(); //que suene
 
-        const start_button = this.add.image(400, 300, "start_button") //A�ado la imagen de start 
-            .setInteractive(true) /*Hago que sea interactivo*/
-            .on/*Hace algo mira el que*/('pointerdown', /*Creo una funcion anonima*/(/*no recibe ningun parametros*/) => {
-                this.sound.play("select"); //que suene el sonido de play
-                //this.scene.stop("IntroScene"); //carga la escena de intro
-                //this.scene.start("GameScene"); //carga la escena de game
+        const start_button = this.add.image(400, 300, "start_button")
+        .setInteractive()
+        .on('pointerdown',() => {
+                this.sound.play('select'); //que suene el sonido de play
+                this.scene.stop('MenuInicio'); //carga la escena de intro
+                this.scene.start('Juego'); //carga la escena de game
         });
-        //const config_button = this.add.image(400, 375, "config_button");
-        //const help_button = this.add.image(400, 450, "help_button");
+        const salir = this.add.image(400, 450, 'salir');
+        
+        salir.setInteractive().on('pointerdown', () => {
+            console.log('Botón de Salir clickeado');
+            // Intentar cerrar la ventana si es posible
+            window.close();
+        });
+        const opciones = this.add.image(400, 375, 'opciones');
         
 
         this.events.on('shutdown', () => { this.bgMusic.stop(); }); 
