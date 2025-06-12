526d73ad3fcdbbff93d7d5b17dbb987360ced0b9
Albi52 <pozvel@gmail.com>
2025-06-12 16:58:48 +0200

    Arreglos

--- API REST/demorobotcolliseum/src/main/java/es/urjc/tchs/demorobotcolliseum/Chat/ChatController.java
+++ API REST/demorobotcolliseum/src/main/java/es/urjc/tchs/demorobotcolliseum/Chat/ChatController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +34,8 @@ public class ChatController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @PostMapping("/{username}/chat")
-    public void postMessage(@PathVariable String username, @RequestParam String message) {
+    @PostMapping("/{username}")
+    public void postMessage(@PathVariable String username, @RequestBody String message) {
         chatService.addMessage(username, message);
     }
 }
