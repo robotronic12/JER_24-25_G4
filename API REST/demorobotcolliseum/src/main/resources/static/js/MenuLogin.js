e2e1de0560b8657a3470e2bac6c4671acfbffaf1
Alba Poza Vela <125303353+Albi52@users.noreply.github.com>
2025-06-16 21:24:40 +0200

    TextosArreglados

--- API REST/demorobotcolliseum/src/main/resources/static/js/MenuLogin.js
+++ API REST/demorobotcolliseum/src/main/resources/static/js/MenuLogin.js
@@ -20,7 +20,7 @@ class MenuLogin extends Phaser.Scene {
         //pausamos el menu inicio hasta que no ha terminado de logearse
         this.scene.pause('MenuInicio');
         //texto dentro del formulario
-        const textFormulario = this.add.text(10, 15, 'Inicia sesion para jugar', { color: 'white', fontFamily: 'Poppins', fontSize: '32px ',stroke: 'black',strokeThickness: 3  });
+        const textFormulario = this.add.text(10, 15, 'Inicia sesion para jugar', { color: 'white', fontSize: '32px ',stroke: 'black',strokeThickness: 3  });
         //Añado el html
         const elementDOM = this.add.dom(400, 350).createFromCache('registro');
         //element.addListener('click');
@@ -28,7 +28,7 @@ class MenuLogin extends Phaser.Scene {
         var ChangeButton = elementDOM.getChildByID('changeScreen');
         const actualizarFormulario = function () {
             if (registro_pantalla) {
-                textFormulario.setText('Registrate poniendo usuario y contraseña');
+                textFormulario.setText('Registrate poniendo usuario y contrasena');
                 elementDOM.getChildByID('submit').innerText = 'REGISTRATE';
                 elementDOM.getChildByID('changeScreen').innerText = '¿Ya tiene usuario? Logueate';
             }
@@ -70,7 +70,7 @@ class MenuLogin extends Phaser.Scene {
                 })
                 .then(response => {
                     if (!response.ok) {
-                        throw new Error('Error en la autenticación, error al registrar usuario');
+                        throw new Error('Error en la autenticacion, error al registrar usuario');
                     }
                     registro_pantalla = false;    //vamos a la pantalla de login
                     actualizarFormulario();
